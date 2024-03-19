93e38c020b515da533d1d0a53546ea4d52a58668
LAPTOP-F1TJ8HUE\rebecca moshabane <karabopaile14@gmail.com>
2024-03-19 20:12:26 +0200

    First commit

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+let sign = ">";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {alert("Hello User!")}
+      <Text style={{color: 'rgb(255,255,255)'}}>
+        C:\Users\Users{sign} Please enter item: 
+      
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgb(0,0,0)',
     alignItems: 'center',
     justifyContent: 'center',
   },
